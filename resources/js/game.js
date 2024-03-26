87c04b29e74c6af341a31b00a8a9b951f8de5c6d
Haroldas <hpharis258@gmail.com>
2024-03-26 21:56:31 +0000

    Adding game logic

--- resources/js/game.js
+++ resources/js/game.js
@@ -12,9 +12,9 @@ async function getNextQuote() {
     quoteDisplayElement.innerText = quote;
 }
 
-getNextQuote().then(() => {
-    addLetterClassesToWords();
-});
+// getNextQuote().then(() => {
+//     addLetterClassesToWords();
+// });
 
 function formatWord(word){
     return `<div class="word" style="margin: 5px"><span class="letter">${word.split('').join('</span><span class="letter">')}</span></div>`;
@@ -27,6 +27,7 @@ function addLetterClassesToWords(){
         words[i] = formatWord(words[i]);
     }
     document.getElementById('wordsToType').innerHTML = words.join(' ');
+    // Add classes to the first word and letter
     $('.word').first().addClass('current-word');
     $('.letter').first().addClass('current-letter');
 }
@@ -34,12 +35,70 @@ function addLetterClassesToWords(){
 // Add Keyup event listener 
 document.getElementById("GameArea").addEventListener("keyup", function(event) {
     const key = event.key;
-    const currentLetter = document.querySelector('.current-letter');
-    const expectedLetter = currentLetter.innerHTML;
+    let currentLetter = document.querySelector('.current-letter');
+    if(currentLetter === null)
+    {
+        // Space
+        currentLetter = "";
+    }
+    console.log(currentLetter);
+    const expectedLetter = currentLetter?.innerHTML == null ? ' ' : currentLetter.innerHTML;
+    alert("Expected Letter: " + expectedLetter);
     const currentWord = document.querySelector('.current-word');
+    const isLetter = key.length === 1 && key !== ' ';
+    const isSpace = key === ' ';
+    if(isLetter)
+    {
+        if(currentLetter)
+        {
+            if(key === expectedLetter)
+            {
+                // if key correct add correct class to the letter and move to the next letter
+                currentLetter.classList.add('correct');
+                currentLetter.classList.remove('current-letter');
+                if(currentLetter.nextElementSibling !== null)
+                {
+                    currentLetter.nextElementSibling.classList.add('current-letter');
+                }
+                
+            }
+            else
+            {
+                // if key incorrect add incorrect class to the letter
+                currentLetter.classList.add('incorrect');
+            }
+        } 
+    }
+    if(isSpace)
+    {
+        alert("Current Letter: " + currentLetter);
+        // Check if on the last letter of the word and if so move to the next word
+        if(currentLetter === currentWord.lastElementChild)
+        {
+            alert('On the last letter of the word lets move to the next word');
+            currentWord.classList.remove('current-word');
+            currentWord.nextElementSibling.classList.add('current-word');
+            currentLetter = currentWord.firstChild;
+            console.log(currentLetter);
 
-
+        }
+        if(expectedLetter !== ' ')
+        {
+            // space not expexted so add incorrect class to the letter and move to the next letter
+            currentLetter.classList.add('incorrect');
+            currentLetter.classList.remove('current-letter');
+            currentLetter.nextElementSibling.classList.add('current-letter');
+        }
+    }
     console.log(event.key, expectedLetter);
 });
+
+function Game(){
+    getNextQuote().then(() => {
+        addLetterClassesToWords();
+    });
+}
+
+Game();
 
 //addLetterClassesToWords();
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,8 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Rubik+Scribble&family=Workbench:SCAN@-12&display=swap" rel="stylesheet">
+
 @vite(['resources/js/game.js'])
 @if(Auth::check()) 
         <x-app-layout>
@@ -19,11 +23,15 @@
 @else 
     <x-guest-layout>
     
-    <div id="wordPerMinuteArea"><h1>WPM: 0</h1></div>
-    <div id="timer">Time Left: 30</div>
+    <div id="game-information">
+        <div id="timer">Time Left: 30</div>
+        <div id="wordPerMinuteArea"><h1>WPM: 0</h1></div>
+    </div>
+   
     <div id="GameArea" tabindex="0">
         <div id="wordsToType">Loading Quote...</div>
-        <div id="focusError">Click on here to start typing!</div>
+        <div id="cursor"></div>
+        <div id="focusError">Click here to start typing!</div>
     </div>
   
     </x-guest-layout>
